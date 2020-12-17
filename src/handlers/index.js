8c417e10662b8bc4f173fad6dd2f18e4e30b7b22
ben.oxmo <39051980+benoxmo@users.noreply.github.com>
2020-12-17 11:49:36 +0100

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,13 +1,13 @@
 const addAddressHandler = require('./addAddress')
 const helpHandler = require('./help')
 const signupHandler = require('./signup')
-const xpHandler = require('./xp')
+const xpHandler = require('./cred')
 
 const handlers = new Map([
   ['addaddress', addAddressHandler],
   ['help', helpHandler],
   ['signup', signupHandler],
-  ['xp', xpHandler],
+  ['cred', xpHandler],
 ])
 
 module.exports = handlers

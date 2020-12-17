297cd896878734900d742939c973e6b346235d60
ben.oxmo <39051980+benoxmo@users.noreply.github.com>
2020-12-17 11:59:19 +0100

    Update help.js

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -1,25 +1,13 @@
-const helpContent = `
-/* Available AracredBot commands:
-
- - !ac addaddress <address> → Links your Discord account with the Ethereum address <address>. Example:
-
-> !ac addaddress 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B
-
-- !ac help → This command.
-
-- !ac signup <platform1/username> <platform2/username>... → Links your Discord account with platforms <platform1/username> <platform2/username>... to be able to gain cred. Supported platforms: github, discourse. NOTE: Discord will be linked to your username automatically.
-
-> !ac signup github/foo discourse/foo */
-
-Welcome to the SourceCred Discord! I know bots aren’t advanced enough to have feelings (yet), but I’m still really glad you’re here. I’d love to give you some next steps to connect you with the community and to get you earning Cred! Here are some **commands** to get you started:
+const helpContent = `, welcome to the SourceCred Discord! 
+I know bots aren’t advanced enough to have feelings (yet), but I’m still really glad you’re here. I’d love to give you some next steps to connect you with the community and to get you earning Cred! Here are some **commands** to get you started:
            
             - **!read** // docs curated by the community that provide insight on the project
                 
             - **!listen** // voices from the community 
              
-            - **!watch** //  videos that make additional entry to your diving in SourceCred
+            - **!watch** //  videos to make your SourceCred diving easier 
 
-            - **!cred** & *[discord-userrname]* // it returns your cred balance from the ledger
+            - **!cred** & *@[discord-userrname]* // it returns your cred balance from the ledger
 
 > !cred @discord-username
 

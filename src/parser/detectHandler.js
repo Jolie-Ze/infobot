3656ee88f6dcdafde84b702c25f58b939e044f85
ben.oxmo <39051980+benoxmo@users.noreply.github.com>
2021-01-12 13:35:19 +0100

    Revert "Update detectHandler.js"
    This reverts commit 6f1d11c4d9ff76299e3b00ea426eb40ede097297.

--- src/parser/detectHandler.js
+++ src/parser/detectHandler.js
@@ -4,7 +4,7 @@ const handlers = require('../handlers/index')
 const noop = () => undefined
 
 const detectHandler = (message) => {
-  const [requestedNamespace, requestedHandler] = message.split('')
+  const [requestedNamespace, requestedHandler] = message.split(' ')
   // If it's not a flag, we can safely ignore this command.
   if (!requestedNamespace.startsWith('!') && !requestedNamespace.startsWith('!cred')) {
     return noop()

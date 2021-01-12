200a1fea2e1bb489005372f09b3fed13596f6c9f
ben.oxmo <39051980+benoxmo@users.noreply.github.com>
2021-01-12 13:34:49 +0100

    Revert "Update help.js"
    This reverts commit 2903115bd6e84aa9a4e27a8adba71e12aacb1f49.

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -1,11 +1,11 @@
 const helpContent = ` welcome to the SourceCred Discord! 
 I know bots aren’t advanced enough to have feelings (yet), but I’m still really glad you’re here. I’d love to give you some next steps to connect you with the community and to get you earning Cred! Here are some **commands** to learn about SourceCred:
            
-            - **! read** // docs curated by the community that provide additional insight
+            - **!ac read** // docs curated by the community that provide additional insight
                 
-            - **! listen** // get to hear people from the community 
+            - **!ac listen** // get to hear people from the community 
              
-            - **! watch** //  videos to dive into SourceCred 
+            - **!ac watch** //  videos to dive into SourceCred 
 
             - **!cred** & *@[discord-userrname]* // it returns your cred balance from the ledger
 

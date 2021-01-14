60c14630358c8f8c8579db9a3c76d51e1be41205
omyzmo <74498378+omyzmo@users.noreply.github.com>
2021-01-14 14:20:52 +0100

    Update help.js

--- src/handlers/help.js
+++ src/handlers/help.js
@@ -1,11 +1,11 @@
 const helpContent = ` welcome to the SourceCred Discord! 
 I know bots aren’t advanced enough to have feelings (yet), but I’m still really glad you’re here. I’d love to give you some next steps to connect you with the community and to get you earning Cred! Here are some **commands** to learn about SourceCred:
            
-            - **!ac read** // docs curated by the community that provide additional insight
+            - **!read** // docs curated by the community that provide additional insight
                 
-            - **!ac listen** // get to hear people from the community 
+            - **!listen** // get to hear people from the community 
              
-            - **!ac watch** //  videos to dive into SourceCred 
+            - **!watch** //  videos to dive into SourceCred 
 
             - **!cred** & *@[discord-userrname]* // it returns your cred balance from the ledger
 

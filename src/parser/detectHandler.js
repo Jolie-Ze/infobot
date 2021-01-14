8684e209c71f83f9830ba3e51187d5840ecb39b0
omyzmo <74498378+omyzmo@users.noreply.github.com>
2021-01-14 11:20:47 +0100

    Update detectHandler.js

--- src/parser/detectHandler.js
+++ src/parser/detectHandler.js
@@ -4,31 +4,33 @@ const handlers = require('../handlers/index')
 const noop = () => undefined
 
 const detectHandler = (message) => {
-  const [requestedNamespace, requestedHandler] = message.split(' ')
+
   // If it's not a flag, we can safely ignore this command.
-  if (!requestedNamespace.startsWith('!') && !requestedNamespace.startsWith('!cred')) {
+  if (!requestedNamespace.startsWith('!')) {
     return noop()
   }
 
   // Filter the standalone cred handler to only allow !cred instead of !ac cred
-  if (requestedNamespace === '!' && requestedHandler === 'cred') {
+  /*if (requestedNamespace === '!' && requestedHandler === 'cred') {
     throw new RequestHandlerError(
       `could not find command with flag ${requestedHandler}`,
     )
-  }
+  }*/
 
   let receivedHandler = null
 
   if (requestedNamespace.startsWith('!')) {
-    receivedHandler = handlers.get(requestedHandler)
-  } else if (requestedNamespace.startsWith('!cred')) {
+    receivedHandler = handlers.get(requestedHandler.substring(1))
+  
+}
+/* else if (requestedNamespace.startsWith('!cred')) {
     receivedHandler = handlers.get(requestedNamespace.substring(1))
   }
   if (requestedNamespace !== '!' && requestedNamespace !== '!cred') {
     throw new RequestHandlerError(
       `Could not find command with flag ${requestedNamespace}`,
     )
-  }
+  }*/
 
   if (typeof receivedHandler !== 'function') {
     throw new RequestHandlerError(
